44528588417ecba5a7131df05ee656abf01ba677
Timothée DURAND <timothee.durand70@gmail.com>
2023-03-23 10:53:51 +0100

    add front service

--- frontend/src/API.js
+++ frontend/src/API.js
@@ -1,13 +1,19 @@
 import axios from "axios";
 
+const apiClient = axios.create({
+    baseURL: "http://localhost:4001",
+})
+
+
 export const getTodos = async () => {
-  //TO_MODIFY
+    return apiClient.get("/todos");
 };
 
 export const createTodo = async (formData) => {
-  //TO_MODIFY
+    console.log(formData)
+   await apiClient.post("/todos", formData)
 };
 
 export const deleteTodo = async (todoId) => {
-  //TO_MODIFY
+  await apiClient.delete(`/todos/${todoId}`)
 };
--- frontend/src/components/AddTodo.jsx
+++ frontend/src/components/AddTodo.jsx
@@ -14,12 +14,16 @@ const AddTodo = ({ saveTodo }) => {
     <form className="Form" onSubmit={(e) => saveTodo(e, formData)}>
       <div>
         <div>
-          <label htmlFor="name">Name</label>
-          <input onChange={handleForm} type="text" id="name" />
+          <label htmlFor="title">Name</label>
+          <input onChange={handleForm} type="text" id="title" />
         </div>
         <div>
           <label htmlFor="description">Description</label>
           <input onChange={handleForm} type="text" id="description" />
+        </div>
+        <div>
+          <label htmlFor="due_date">Date</label>
+          <input onChange={handleForm} type="date" id="due_date" />
         </div>
       </div>
       <button disabled={formData === undefined ? true : false}>Add Todo</button>
