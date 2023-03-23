6da957b0372e2227f6388086bce83525be17f4e2
Timothée DURAND <timothee.durand70@gmail.com>
2023-03-23 10:33:33 +0100

    add db

--- backend/src/db.config.js
+++ backend/src/db.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  HOST: "TO_MODIFY",
-  USER: "TO_MODIFY",
-  PASSWORD: "TO_MODIFY",
-  DATABASE: "TO_MODIFY",
-  PORT: "TO_MODIFY",
+  HOST: "db",
+  USER: "app",
+  PASSWORD: "secret",
+  DATABASE: "app",
+  PORT: "3306",
 };
--- backend/src/index.js
+++ backend/src/index.js
@@ -19,19 +19,58 @@ const port = 4001
 app.use(express.json());
 app.use(cors());
 
+// INIT DB
+const TABLE_NAME = "todo";
+db.schema.hasTable(TABLE_NAME).then(function(exists) {
+  if (!exists) {
+    console.log("CREATING TABLE TODO")
+    return db.schema.createTable(TABLE_NAME, function(t) {
+      t.increments('id').primary();
+      t.string('title', 100);
+      t.text('description');
+      t.date('due_date')
+      t.boolean('done');
+    });
+  }
+});
+
 app.get("/todos", async (req, res) => {
-  //TO_MODIFY
-  res.send([]) // to remove after question 1)
+  const todos = await db.select().table(TABLE_NAME);
+  res.send(todos)
 });
 
 app.post("/todos", async (req, res) => {
-  //TO_MODIFY
+  const body = req.body;
+  if(!body.title || !body.description || !body.due_date) {
+    res.status(400).send({error: "Missing required fields"});
+    return;
+  }
+  const todo = {
+    title: body.title,
+    description: body.description,
+    due_date: body.due_date,
+    done: false,
+  }
+  try {
+    const result = await db(TABLE_NAME).insert(todo);
+    res.send(result);
+  } catch (e) {
+    console.error(e)
+    res.status(500).send({error: "Internal server error", e});
+  }
 });
 
 app.delete("/todos/:todoId", async (req, res) => {
-  //TO_MODIFY
+  const todoId = req.params.todoId;
+  try {
+    await db(TABLE_NAME).where({id: todoId}).del()
+    res.send({success: true});
+  } catch (e) {
+    console.error(e)
+    res.status(500).send({error: "Internal server error", e});
+  }
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Listening on port ${port}`);
 });
